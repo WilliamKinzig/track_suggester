1591515c9568abdd2b721ef75198e4567078e43f
William Kinzig <wippawill2@gmail.com>
2018-06-01 10:06:11 -0700

    begin basic layout of question (y/n), and prepare to builde js and css pages

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,29 @@
 $(document).ready(function() {
+
+/*
   var age = parseInt(prompt("How old are you?"));
 
-  if (age >= 21) {
+  var question01 = parseInt(prompt("How old are you?"));
+  var question02 = parseInt(prompt("How old are you?"));
+  var question03 = parseInt(prompt("How old are you?"));
+  var question04 = parseInt(prompt("How old are you?"));
+  var question05 = parseInt(prompt("How old are you?"));
+
+
+
+  if (age > 21) {
     $('#drinks').show();
+  } else if (age === 21) {
+    alert("Now don't go crazy!");
+    $('#drinks').show();
+  } else {
+    $('#under-21').show();
   }
 });
+
+*/
+var answer1 = $("input:radio[name=CSS]:checked").val();
+var answer2 = $("input:radio[name=c#]:checked").val();
+var answer3 = $("input:radio[name=Java]:checked").val();
+var answer4 = $("input:radio[name=PHP]:checked").val();
+var answer5 = $("input:radio[name=RUBY]:checked").val();
