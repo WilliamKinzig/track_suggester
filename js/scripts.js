b426ef610e1ddafcaf67f40cb0a24eb3ed041be3
William Kinzig <wippawill2@gmail.com>
2018-06-01 12:10:27 -0700

    attemp to make the check boxes and the submit button work

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,7 @@
+.btn {
+  display: block;
+}
 
-$(document).ready(function() {
-
-  var courseSuggestion = $("input:radio[name=course]:checked").val();
-
-  if (courseSuggestion === "1") {
-    $('#weRecommend').show();
-    $('#resultCSS').show();
-
-  }
+#quote {
+  display: none;
 }
